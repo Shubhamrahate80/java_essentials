9879cb275a962ebb28b1b226ed6ea1cce8145a95
Shubhamrahate80 <shubhamrahate325@gmail.com>
2023-11-20 13:19:25 +0530

    third commit

---
+++ swap1.java
@@ -0,0 +1,16 @@
+public class swap1 {
+
+    public static void main(String[] args) {
+        int n1=12;
+        int n2=11;
+
+        n1=n1+n2;
+        n2=n2-n1;
+        n1=n1+n2;
+
+        System.out.println(n1);
+        System.out.println(n2);
+
+    }
+    
+}
