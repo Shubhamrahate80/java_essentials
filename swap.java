f9dc8ce46d1ea212b3349c8bc24b105ba5a9d549
Shubhamrahate80 <shubhamrahate325@gmail.com>
2023-11-20 13:15:02 +0530

    second commit

---
+++ swap.java
@@ -0,0 +1,17 @@
+public class swap {
+
+    public static void main(String[] args) {
+        
+        int n1=10;
+        int n2=30;
+
+        int vada=n1;
+
+        n1=n2;
+        n2=vada;
+
+        System.out.println(n1);
+        System.out.println(n2);
+    }
+    
+}
